01b981287f1116c1054707e0462def2b7992b5e8
joraeng <60039286+joraeng@users.noreply.github.com>
2021-02-06 15:25:11 +0900

    clock part2

--- clock.js
+++ clock.js
@@ -5,13 +5,16 @@ function getTime() {
     const date = new Date(); // date는 클래스인데 구체적으로는 따로 공부해야함
     const minutes = date.getMinutes();
     const hours = date.getHours();
-    const secounds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minutes}:${secounds}`;
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours < 10 ? `0${hours}`: hours}:${minutes < 10 ? `0${hours}` : hours}:${
+        seconds < 10 ? `0${seconds}`:seconds
+    }`;
 }
 
 //init이 제일 처음만든 함수인데 어떤 역할을 하는 친구인지 모르겠음
 function init() { 
     getTime();
+    setInterval(getTime, 1000);
  }; 
 
 init(); 
