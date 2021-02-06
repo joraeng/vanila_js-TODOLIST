00c1df30fde090e00aa149f02b0dbabced95daff
joraeng <60039286+joraeng@users.noreply.github.com>
2021-02-06 15:12:37 +0900

    3.1 making a js colck part one

---
+++ clock.js
@@ -0,0 +1,17 @@
+const clockContainer = document.querySelector(".js-clock"),
+clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+    const date = new Date(); // date는 클래스인데 구체적으로는 따로 공부해야함
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const secounds = date.getSeconds();
+    clockTitle.innerText = `${hours}:${minutes}:${secounds}`;
+}
+
+//init이 제일 처음만든 함수인데 어떤 역할을 하는 친구인지 모르겠음
+function init() { 
+    getTime();
+ }; 
+
+init(); 
